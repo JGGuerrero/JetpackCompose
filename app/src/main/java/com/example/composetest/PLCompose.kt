0ac03487c50bd8d91d7f70f88eb1dc242dc136a8
unknown <jgguerrero@mail.fresnostate.edu>
2021-04-25 17:48:26 -0700

    finished image-caard

--- app/src/main/java/com/example/composetest/PLCompose.kt
+++ app/src/main/java/com/example/composetest/PLCompose.kt
@@ -3,20 +3,29 @@ package com.example.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetest.ui.theme.ComposeTestTheme
 
 class PLCompose : ComponentActivity() {
@@ -30,27 +39,62 @@ class PLCompose : ComponentActivity() {
 
 @Composable
 fun Section1() {
-    Column(
-        modifier = Modifier
-            .background(Color.DarkGray)
-            .fillMaxHeight(0.5f)
-            .fillMaxWidth()
-//            .padding(top = 50.dp)
-            .border(5.dp, Color.Magenta)
-            .padding(5.dp)
-            .border(5.dp, Color.Blue)
-            .padding(15.dp)
-            .border(2.dp, Color.Red)
-            .padding(10.dp)
-//            .width(600.dp)
-//            .requiredWidth(600.dp)
-    ) {
-        Text(text = "Hello")
-        Spacer(modifier = Modifier.height(50.dp))
-        Text(text = "World")
+    val painter = painterResource(id = R.drawable.hunting_horn_mizu)
+    val description = "Mizutsune Hunting Horn"
+    val title = "Mizutsune Hunting Horn"
+    Box(modifier = Modifier
+        .fillMaxWidth(0.5f)
+        .padding(16.dp)) {
+        ImageCard(painter = painter, contentDescription = description, title = title)
     }
+
 }
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        // Box allows you to place elements on top of another(stacking)
+        Box(modifier = Modifier.height(200.dp)) {
+            // First element placed in the box is the image (acting as background)
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            // Another box is set inside the first Box. This will allow the inner contents of this Box(Text) to be aligned accordingly without messing with the image alignment
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+    }
+}
 
 
 @Preview(showBackground = true)
